e6dab2173ceae6169ef947a8c1c65288228fbfe0
DaryaBN <darya_bn@bk.ru>
2023-02-16 22:31:46 +0500

    статитстика

---
+++ index.js
@@ -0,0 +1,9 @@
+
+let ПользователейЗарегистрировано = document.querySelector("#ПользователейЗарегистрировано")
+ПользователейЗарегистрировано.textContent=20
+
+let СообщенийНаписано = document.querySelector("#СообщенийНаписано")
+СообщенийНаписано.textContent=556
+
+let НаписаноСегодня = document.querySelector("#НаписаноСегодня")
+НаписаноСегодня.textContent=58
