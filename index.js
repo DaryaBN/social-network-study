5ba9655c176ad851c4fb79c70ee843e45c35af74
DaryaBN <darya_bn@bk.ru>
2023-02-17 01:23:27 +0500

    исправила названия на англ

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-let ПользователейЗарегистрировано = document.querySelector("#ПользователейЗарегистрировано")
-ПользователейЗарегистрировано.textContent=20
+let Registerde = document.querySelector("#Registerde")
+Registerde.textContent=20
 
-let СообщенийНаписано = document.querySelector("#СообщенийНаписано")
-СообщенийНаписано.textContent=556
+let WirteMsg = document.querySelector("#WirteMsg")
+WirteMsg.textContent=556
 
-let НаписаноСегодня = document.querySelector("#НаписаноСегодня")
-НаписаноСегодня.textContent=58
+let НаписаноСегодня = document.querySelector("#WirteMsgTodey")
+WirteMsgTodey.textContent=58
