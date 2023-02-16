ab3c726a31912c19781bed9842d0931c30ec150f
DaryaBN <darya_bn@bk.ru>
2023-02-17 01:40:55 +0500

    забыла в предудущем исправить одно название ветки

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ Registerde.textContent=20
 let WirteMsg = document.querySelector("#WirteMsg")
 WirteMsg.textContent=556
 
-let НаписаноСегодня = document.querySelector("#WirteMsgTodey")
+let WirteMsgTode = document.querySelector("#WirteMsgTodey")
 WirteMsgTodey.textContent=58
