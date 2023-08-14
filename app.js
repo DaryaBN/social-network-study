c4ce813c0a5a859894dfb113150b4027dc9f82a0
DaryaBN <darya_bn@bk.ru>
2023-08-14 14:17:39 +0500

    Express

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import fs from 'fs';
+
+const app = express()
+const port = 3000
+
+const html = fs.readFileSync('public/index.html', 'utf8');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.type('html').send(html));
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
