7135acc8e8925d604ffee82bc3948c6dbaddde83
DaryaBN <darya_bn@bk.ru>
2023-10-25 23:24:33 +0500

    REST API

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import fs from 'fs';
 import pkg from 'pg';
 
-
 const app = express();
 const port = 3000;
 
@@ -35,50 +34,47 @@ let information = await pool.query('SELECT * from posts');
 app.get('/posts', (req, res) => res.type('json').send(information.rows));
 
 app.get('/posts/:id', (req, res) => {
-  information.rows.forEach((item, index) => {
-    if(item.id === req.params.id) {
-     res.type('json').send(item)
-   }
-   });
-  res.send(404)
- });
-
+  information.rows.forEach((item) => {
+    if (item.id === req.params.id) {
+      res.type('json').send(item);
+    }
+  });
+  res.send(404);
+});
 
 app.post('/posts', (req, res) => {
   let NewPost = {
-   "id": +(new Date()),
-   "name" : req.body.name,
-   "nick": req.body.nick,
-   "time": new Date(),
-   "mes":  req.body.mes
-  }
-  information.rows.push(NewPost)
+    id: +(new Date()),
+    name: req.body.name,
+    nick: req.body.nick,
+    time: new Date(),
+    mes: req.body.mes,
+  };
+  information.rows.push(NewPost);
   // console.log(req.body)
-  res.status(201).type('json').send(NewPost)
- });
- 
+  res.status(201).type('json').send(NewPost);
+});
 
- app.delete('/posts/:id', (req, res) => {
-  information.rows.forEach((item, index) => {
-  if(information.rows[index].id === req.params.id) {
-      information.rows.slice(index, 1)
-      res.send(201)
+app.delete('/posts/:id', (req, res) => {
+  information.rows.forEach((index) => {
+    if (information.rows[index].id === req.params.id) {
+      information.rows.slice(index, 1);
+      res.send(201);
     }
-  })
-  console.log(req.body)
-  res.send(404)
- });
+  });
+  console.log(req.body);
+  res.send(404);
+});
+app.post('/posts/:id', (req, res) => {
+  information.rows.forEach((index) => {
+    if (information.rows[index].id === req.params.id) {
+      information.rows[index].mes = req.body.mes;
+      res.type('json').send(information.rows);
+    }
+  });
+  res.send(404);
+});
 
- app.post('/posts/:id', (req, res) => {
-  information.rows.forEach((item, index) => {
-    if(item.id === req.params.id) {
-      item.mes = req.body.mes
-     res.type('json').send(information.rows)
-   }
-   });
-  res.send(404)
- }); //исправление поста не лучше слелать через put
- 
 // let user = {
 //   name: 'John',
 //   surname: 'Smith'
