3c80cb1cb18a2d64c68617f425f7820081640be4
DaryaBN <darya_bn@bk.ru>
2024-07-23 20:19:31 +0500

    app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = 3000;
 const html = fs.readFileSync('index.html', 'utf8');
 app.use(express.json());
 app.use(cookies());
-app.use(express.static('public'));
+app.use(express.static('main'));
 // app.get('/', (req, res) => res.type('html').send(html));
 
 app.listen(port, () => {
