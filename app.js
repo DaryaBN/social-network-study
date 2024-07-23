02b5df9fcab507a7302b2e4910a94214a84c312b
DaryaBN <darya_bn@bk.ru>
2024-07-24 00:57:14 +0500

    app

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ import cookies from 'cookie-parser';
 const app = express();
 const port = 3000;
 
-// const html = fs.readFileSync('index.html', 'utf8');
-// app.use(express.static('public'));
+const html = fs.readFileSync('index.html', 'utf8');
+app.use(express.static('public'));
 app.use(express.json());
 app.use(cookies());
+
+app.get('/', (req, res) => res.type('html').send(html));
+app.get('/app', (req, res) => res.type('html').send(index));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
