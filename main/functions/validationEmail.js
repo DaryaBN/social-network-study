6cdacf1635de5844f14e38f8938609a782456842
DaryaBN <darya_bn@bk.ru>
2024-07-23 20:34:55 +0500

    comm

--- main/functions/validationEmail.js
+++ main/functions/validationEmail.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-useless-escape */
+
 export default function validationEmail(em) {
   let res = (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
   return res.test(String(em).toLowerCase());
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: './public/',
+    outDir: './public',
     emptyOutDir: false,
   },
   resolve: {
