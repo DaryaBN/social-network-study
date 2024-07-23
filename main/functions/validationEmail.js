77ae4c00a1444d5d7018a9dc162f3a9c026cc231
DaryaBN <darya_bn@bk.ru>
2024-07-24 01:55:23 +0500

    app

--- main/functions/validationEmail.js
+++ main/functions/validationEmail.js
@@ -1,9 +1,4 @@
 export default function validationEmail(em) {
-  if( em.includes('@') && em.includes('.ru' || '.com')){
-    return true
-  } else {
-    return false
-  }
-  // let res = (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
-  // return res.test(String(em).toLowerCase());
+  let res = (/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/);
+  return res.test(String(em).toLowerCase());
 }
