f43187ec4411b707f4ef59b35f4fc1d91037e3a8
DaryaBN <darya_bn@bk.ru>
2024-07-24 03:28:52 +0500

    app

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookies());
 
 // app.get('/', (req, res) => res.type('html').send(html));
-app.get('/app', (req, res) => res.type('html').send(index));
+// app.get('/app', (req, res) => res.type('html').send(index));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
